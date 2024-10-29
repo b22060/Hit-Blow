87ae34523295a0d7b443dceea284df82b5b875a0
b22060 <e1b22060@oit.ac.jp>
2024-10-29 16:03:39 +0900

    リポジトリの初期化

---
+++ src/main/java/kmt/hit_blow/HitBlowApplication.java
@@ -0,0 +1,13 @@
+package kmt.hit_blow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitBlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitBlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kmt/hit_blow/HitBlowApplicationTests.java
@@ -0,0 +1,13 @@
+package kmt.hit_blow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitBlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
