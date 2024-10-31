0789584c374ebc52b960c668f182699db414af05
e1b22090 <e1b22090.oit.ac.jp>
2024-11-01 01:58:19 +0900

    work6-2

--- src/main/java/kmt/hit_blow/security/AuthConfiguration.java
+++ src/main/java/kmt/hit_blow/security/AuthConfiguration.java
@@ -1,5 +1,47 @@
 package kmt.hit_blow.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
 public class AuthConfiguration {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.formLogin(login -> login
+        .permitAll())
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/")) // ログアウト後にトップページにリダイレクト
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/hit_blow"))
+            .authenticated()
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/**"))
+            .permitAll())
+        .csrf(csrf -> csrf
+            .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/*")))
+        .headers(headers -> headers
+            .frameOptions(frameOptions -> frameOptions
+                .sameOrigin()));
+    return http.build();
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user1 = User.withUsername("user1")
+        .password("{bcrypt}$2y$05$3Fw6J1urtsQqlyolYGP66eZx.RSKtMUem8orxdrWn3t1XdpUkYJWy").roles("USER").build();
+    UserDetails user2 = User.withUsername("user2")
+        .password("{bcrypt}$2y$05$isYyRzlQ5RZF9zBP2ukJYeCKILLRfb8dqxuRdJCLw2.UDiIADPjOe").roles("USER").build();
+
+    return new InMemoryUserDetailsManager(user1, user2);
+  }
 
 }
