d01aaa6904a47723fcdb9872ff3bb3a410f430e9
e1b22090 <e1b22090.oit.ac.jp>
2024-11-01 02:19:05 +0900

    work6-2(2)

--- src/main/java/kmt/hit_blow/model/Hit_Blow.java
+++ src/main/java/kmt/hit_blow/model/Hit_Blow.java
@@ -1,5 +1,17 @@
 package kmt.hit_blow.model;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class Hit_Blow {
 
+  @GetMapping("/index")
+  public String sample21() {
+    return "index.html";
+  }
 }
