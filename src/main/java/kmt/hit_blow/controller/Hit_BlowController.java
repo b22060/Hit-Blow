6e9b5c2feaebabcbbc4585532c16c0556cabee26
e1b22090 <e1b22090.oit.ac.jp>
2024-11-02 01:37:54 +0900

    work6-2

--- src/main/java/kmt/hit_blow/controller/Hit_BlowController.java
+++ src/main/java/kmt/hit_blow/controller/Hit_BlowController.java
@@ -1,5 +1,18 @@
 package kmt.hit_blow.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.ui.Model;
+
+@Controller
 public class Hit_BlowController {
+
+  @GetMapping("/hit-blow")
+  public String sample31() {
+    return "hit-blow.html";
+  }
 
 }
