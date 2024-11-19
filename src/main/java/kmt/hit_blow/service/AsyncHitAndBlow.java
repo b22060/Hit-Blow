0eafe06ad16c809c5585bc181192f9bdde790f90
e1b22090 <e1b22090.oit.ac.jp>
2024-11-19 18:01:30 +0900

    sampleSSEの実装

--- src/main/java/kmt/hit_blow/service/AsyncHitAndBlow.java
+++ src/main/java/kmt/hit_blow/service/AsyncHitAndBlow.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -15,6 +16,8 @@ public class AsyncHitAndBlow {
   private int customerCount = 1;// customerロール用カウンター
   private int sellerCount = 1;// sellerロール用カウンター
   private final Logger logger = LoggerFactory.getLogger(AsyncHitAndBlow.class);
+
+  private int hogehoge = 0;// ０→1→0と遷移する
 
   @Async
   public void count(SseEmitter emitter, String role) throws IOException {
@@ -38,6 +41,40 @@ public class AsyncHitAndBlow {
         TimeUnit.SECONDS.sleep(1);
       }
     } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void samplechange() {
+    if (hogehoge == 0) {
+      hogehoge = 1;
+    } else {
+      hogehoge = 0;
+    }
+  }
+
+  @Async
+  public void sample(SseEmitter emitter) {
+
+    logger.info("AsyncCount58.count");
+    try {
+      while (true) {
+        if (hogehoge == 1) {
+          TimeUnit.MILLISECONDS.sleep(50);
+          continue;
+        }
+        TimeUnit.MILLISECONDS.sleep(50);
+        if (hogehoge == 0) {
+
+          emitter.send("SSEを通信開始");
+          System.out.println("確認用！！！！！");
+          TimeUnit.MILLISECONDS.sleep(5);
+          this.samplechange();
+        }
+
+        TimeUnit.SECONDS.sleep(1);
+      }
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
