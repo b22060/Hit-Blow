c5d5937e0963dc6e710fc3fa73c84086986b86e1
b22060 <e1b22060@oit.ac.jp>
2024-11-22 23:25:01 +0900

    履歴を全て表示可能

--- src/main/java/kmt/hit_blow/model/MatchMapper.java
+++ src/main/java/kmt/hit_blow/model/MatchMapper.java
@@ -16,12 +16,15 @@ public interface MatchMapper {
   @Select("SELECT * FROM matches WHERE matchid = #{matchid}")
   Match selectMatchById(int matchid);
 
+  @Select("SELECT userid2 FROM matches WHERE userid1 = #{userid1} and matchid = #{matchid}")
+  int selectopponentsIdById(int userid1, int matchid);// 対戦相手のidを取得
+
   @Select("SELECT matchid FROM matches WHERE userid1 = #{userid1} and userid2 = #{userid2} ORDER BY matchid DESC LIMIT 1")
-  //useridからmatchidを入手
-  int selectMatchIdByuserId(int userid1,int userid2);
+  // useridからmatchidを入手
+  int selectMatchIdByuserId(int userid1, int userid2);
 
   @Insert("INSERT INTO matches (userid1,userid2,usernum1,usernum2,judge) VALUES (#{userid1},#{userid2},#{usernum1},#{usernum2},#{judge});")
-  void insertMatch(Match match);//optionsは消したおそらくmatchidとかにしたら行けるはず
+  void insertMatch(Match match);// optionsは消したおそらくmatchidとかにしたら行けるはず
 
   @Update("UPDATE matches SET judge=#{judge} WHERE matchid = #{matchid}")
   void updateById(Match match);
